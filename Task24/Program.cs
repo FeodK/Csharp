5e2dad1bcf5f8c7d7f9b135a2684409c59452319
Feodor <mr.fedor.kiselev@gmail.com>
2023-08-25 10:23:22 +0300

    Task24

--- Task24/Program.cs
+++ Task24/Program.cs
@@ -4,9 +4,22 @@
 */
 Console.WriteLine("Введите число: ");
 int number = Convert.ToInt32(Console.ReadLine());
-int sum = 0;
-for(int i=1; i<=number; i++)
+
+// int sum = 0;
+// for(int i=1; i<=number; i++)
+// {
+//     sum += i;
+// }
+// System.Console.WriteLine(sum);
+
+int SumOfNumber(int number)
 {
+    int sum = 0;
+    for(int i=1; i<=number; i++)
+    {
     sum += i;
+    }
+    return sum;
 }
-System.Console.WriteLine(sum);
+
+System.Console.WriteLine(SumOfNumber(number));
