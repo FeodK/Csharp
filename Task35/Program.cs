42807a4c3015c4642b99802b71d28bb189ff5bda
Feodor <mr.fedor.kiselev@gmail.com>
2023-08-29 12:09:59 +0300

    Task35

---
+++ Task35/Program.cs
@@ -0,0 +1,60 @@
+﻿/*
+ Задайте одномерный массив из 123 случайных чисел.
+  Найдите количество элементов массива, значения которых лежат в отрезке [10,99]. 
+Пример для массива из 5, а не 123 элементов. В своем решении сделайте для 123
+[5, 18, 123, 6, 2] -> 1
+[1, 2, 3, 6, 2] -> 0
+[10, 11, 12, 13, 14] -> 5
+*/
+
+Console.Clear();
+
+System.Console.WriteLine("Введите число кол-во элементов массива: ");
+int usNumber = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Задайте границы массива: ");
+int uMin = Convert.ToInt32(Console.ReadLine());
+int uMax = Convert.ToInt32(Console.ReadLine());
+
+
+int[] GetRandomArray(int number, int min, int max)
+{
+    int[] array = new int[number];
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(min, max);
+    }
+    return array;
+}
+
+int[] uArray = GetRandomArray(usNumber, uMin, uMax);
+
+
+void PrintArray(int[] arrayToPrint)
+{
+   System.Console.Write("[ ");
+   for (int i = 0; i < arrayToPrint.Length; i++)
+   {
+    System.Console.Write(arrayToPrint[i]);
+    if (i<arrayToPrint.Length - 1)
+       {
+        Console.Write (arrayToPrint[i] + ", ");
+       }
+   }
+   System.Console.Write("]");
+}
+
+int QuantityNumber(int[] array)
+{
+    int count =0;
+    for (int i = 0; i <array.Length; i++)
+    {
+        if(array[i] <= 99 && array[i] >= 10)
+        {
+            count += 1;
+        }
+    }
+    return count;
+}
+int sum = QuantityNumber(uArray);
+System.Console.WriteLine(sum);
