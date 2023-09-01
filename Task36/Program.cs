1d57430b91c136d037a5c4a5c30bbf6ca7206eac
Feodor <mr.fedor.kiselev@gmail.com>
2023-09-01 09:56:08 +0300

    Task36

--- Task36/Program.cs
+++ Task36/Program.cs
@@ -46,7 +46,7 @@ void PrintArray(int[] arrayToPrint)
 int SumNumber(int[] array)
 {
     int sum = 0;
-    for (int i = 0; i < array.Length; i+=2)
+    for (int i = 1; i < array.Length; i+=2)
     {
         sum = sum + array[i];
     }
