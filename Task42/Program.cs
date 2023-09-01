fec946ace9bab91254b0a313dc4150c9f90e36d5
Feodor <mr.fedor.kiselev@gmail.com>
2023-09-01 11:10:49 +0300

    Task42

---
+++ Task42/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+Напишите программу,
+ которая будет преобразовывать десятичное число в двоичное.
+45 -> 101101
+3 -> 11
+2 -> 10
+*/
+
+Console.Clear();
+
+Console.WriteLine("Enter number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+string Number (int a)
+{
+    string res = string.Empty;
+    int temp = 0;
+
+    for(int i =a; i>0; i /=2)
+    {
+        temp = i%2;
+        res = temp + res;
+    }
+
+    return res; 
+}
+
+string result = Number(num);
+
+System.Console.WriteLine(result);
