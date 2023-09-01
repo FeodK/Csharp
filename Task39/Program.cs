c9dde36f829833ffd02869572e943bbbce098c55
Feodor <mr.fedor.kiselev@gmail.com>
2023-09-01 10:29:14 +0300

    Task39

---
+++ Task39/Program.cs
@@ -0,0 +1,68 @@
+﻿/*
+Напишите программу, которая перевернёт одномерный массив
+ (последний элемент будет на первом месте, а первый - на последнем и т.д.)
+
+[1 2 3 4 5] -> [5 4 3 2 1]
+[6 7 3 6] -> [6 3 7 6]
+Комментарий: эту задачу можно решить 2 способами: 
+1) менять числа местами в исходном массиве; 
+2) создать новый массив и в него записать перевёрнутый исходный массив по элементам.
+*/
+
+Console.Clear();
+
+System.Console.WriteLine("Введите число кол-во элементов массива: ");
+int usNumber = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Задайте границы массива: ");
+int uMin = Convert.ToInt32(Console.ReadLine());
+int uMax = Convert.ToInt32(Console.ReadLine());
+
+
+int[] GetRandomArray(int number, int min, int max)
+{
+    int[] array = new int[number];
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(min, max);
+    }
+    return array;
+}
+
+int[] uArray = GetRandomArray(usNumber, uMin, uMax);
+
+
+void PrintArray(int[] arrayToPrint)
+{
+   System.Console.Write("[ ");
+   for (int i = 0; i < arrayToPrint.Length; i++)
+   {
+    System.Console.Write(arrayToPrint[i]);
+    if (i<arrayToPrint.Length - 1)
+       {
+        Console.Write ( ", ");
+       }
+   }
+   System.Console.Write("]");
+}
+
+int[] ReverseArray(int[] array)
+{
+    int tmp =0;
+    for(int i=0; i<array.Length/2; i++)
+    {
+        tmp = array[i];
+        array[i] = array[array.Length - 1 - i];
+        array[array.Length - 1 - i] = tmp;
+    }
+    return array;
+}
+
+Console.ForegroundColor=ConsoleColor.Green;
+PrintArray(uArray);
+int[] turnArray = ReverseArray(uArray);
+Console.ForegroundColor=ConsoleColor.Blue;
+System.Console.WriteLine();
+PrintArray(turnArray);
+
+
