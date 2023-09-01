e40c2eb319fb4bde05c384add10c119b8e6e4c25
Feodor <mr.fedor.kiselev@gmail.com>
2023-09-01 10:56:24 +0300

    Task40

---
+++ Task40/Program.cs
@@ -0,0 +1,45 @@
+﻿/*
+Напишите программу, которая принимает на вход три числа и проверяет,
+ может ли существовать треугольник с сторонами такой длины.
+*/
+
+Console.WriteLine("Введите три числа: ");
+
+int[] FillArray()
+{
+    int[] array = new int[3];
+    for (int i = 0; i<array.Length; i++)
+    {
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+void PrintArray(int[] arrayToPrint)
+{
+   System.Console.Write("[ ");
+   for (int i = 0; i < arrayToPrint.Length; i++)
+   {
+    System.Console.Write(arrayToPrint[i]);
+    if (i<arrayToPrint.Length - 1)
+       {
+        Console.Write ( ", ");
+       }
+   }
+   System.Console.Write("]");
+}
+
+bool Existence(int[] array)
+{
+    
+    if(array[0] + array[1] > array[2]
+    && array[1] < array[0] + array[2]
+    && array[0] < array[1] + array[2]) return true;
+    else return false;
+    
+}
+int[] uArray = FillArray();
+bool result = Existence(uArray);
+PrintArray(uArray);
+System.Console.WriteLine();
+System.Console.WriteLine(result);
