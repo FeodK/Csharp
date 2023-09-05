7649d218edc95b83bc5b20c7a660e98e2ddac205
Feodor <mr.fedor.kiselev@gmail.com>
2023-09-05 11:44:16 +0300

    Task50

---
+++ Task50/Program.cs
@@ -0,0 +1,64 @@
+﻿/*
+Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
+ и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+17 -> такого числа в массиве нет
+*/
+
+Console.Clear();
+
+int[,] GetRandom2DArray(int rowNumber, int colNumber, int deviation)
+{
+    int[,] result = new int[rowNumber, colNumber];
+    for(int i=0; i<rowNumber; i++)
+    {
+        for (int j = 0; j < colNumber; j++)
+        {
+            result[i,j] = new Random().Next(0, deviation);
+        }
+    }
+    return result;
+}
+
+void Print2DArray(int[,] arrayToPrint)
+{
+    System.Console.Write($"[]\t");
+    for (int i = 0; i < arrayToPrint.GetLength(1); i++)
+    {
+        System.Console.Write($"[{i}]\t");
+    }
+    System.Console.WriteLine();
+    for (int i = 0; i < arrayToPrint.GetLength(0); i++)
+    {
+        System.Console.Write($"[{i}]\t");        
+        for (int j = 0; j < arrayToPrint.GetLength(1); j++)
+        {
+            System.Console.Write(arrayToPrint[i,j] + "\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+void FindEl(int[,] array)
+{
+   
+    Console.WriteLine("Введите позицию элемента: ");
+    int a = Convert.ToInt32(Console.ReadLine());
+    int b = Convert.ToInt32(Console.ReadLine());
+    if (a>array.GetLength(0) || b>array.GetLength(1))
+    {
+        Console.WriteLine("такого числа в массиве нет");
+    }
+    else
+    {
+        int number = array[a,b];
+        Console.WriteLine(number);
+    }
+}
+int[,] randomArray = GetRandom2DArray(5,5,10);
+Print2DArray(randomArray);
+FindEl(randomArray);
