3e1a0f243cb23169f2ea8a566c3cef6081086516
Feodor <mr.fedor.kiselev@gmail.com>
2023-09-05 10:38:39 +0300

    Task48

---
+++ Task48/Program.cs
@@ -0,0 +1,41 @@
+﻿/*
+Задайте двумерный массив размера m на n,
+ каждый элемент в массиве находится по формуле:
+ Aₘₙ = m+n. Выведите полученный массив на экран.
+*/
+Console.Clear();
+
+int[,] GetRandom2DArray(int rowNumber, int colNumber)
+{
+    int[,] result = new int[rowNumber, colNumber];
+    for(int i=0; i<rowNumber; i++)
+    {
+        for (int j = 0; j < colNumber; j++)
+        {
+            result[i,j] = i+j;
+        }
+    }
+    return result;
+}
+
+void Print2DArray(int[,] ArrayToPrint)
+{
+    System.Console.Write($"[]\t");
+    for (int i = 0; i < ArrayToPrint.GetLength(1); i++)
+    {
+        System.Console.Write($"[{i}]\t");
+    }
+    System.Console.WriteLine();
+    for (int i = 0; i < ArrayToPrint.GetLength(0); i++)
+    {
+        System.Console.Write($"[{i}]\t");        
+        for (int j = 0; j < ArrayToPrint.GetLength(1); j++)
+        {
+            System.Console.Write(ArrayToPrint[i,j] + "\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int[,] randomArray = GetRandom2DArray(5,5);
+Print2DArray(randomArray);
