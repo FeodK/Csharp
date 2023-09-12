3e668122c12f8bfbb1eba7c7d60ab9b481abcbd1
Feodor <mr.fedor.kiselev@gmail.com>
2023-09-12 10:58:51 +0300

    Task69

---
+++ Task69/Program.cs
@@ -0,0 +1,14 @@
+﻿/*
+Напишите программу, которая на вход принимает два числа A и B,
+ и возводит число А в целую степень B с помощью рекурсии.
+*/
+int result = SumNumber(2,5);
+
+int SumNumber (int n, int m)
+{   
+    if (m==1)
+    return n;
+    else
+    return n*SumNumber(n, m-1);
+}
+System.Console.WriteLine(result);
